2948aac0c5beec558cf51930ebe3507fe691f752
Your Name <you@example.com>
2022-05-12 18:00:36 +0900

    Modified templates to display posts from database.

--- django_app/settings.py
+++ django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3ou0r3akx!y%v%9^%&es+pj#9y1@_au2wf0041#$tvm(_r6!*r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
 
 
 # Application definition
