ddce06a3c040ba67a9ac75a4c9ef9d4487630e51
Your Name <you@example.com>
2022-06-02 14:33:10 +0900

    additional files and changes for Heroku

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/',views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/new/',views.post_new, name='post_new'),
+    path('drafts/' ,views.post_list, name='post_draft_list'),
 ]
--- django_app/settings.py
+++ django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3ou0r3akx!y%v%9^%&es+pj#9y1@_au2wf0041#$tvm(_r6!*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
-
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'id6447',
+        'USER': 'id6447',
+        'PASSWORD':'id6447',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -112,10 +117,13 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
